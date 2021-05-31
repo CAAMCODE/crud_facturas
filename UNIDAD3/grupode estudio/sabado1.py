252d8f5d90785453832b47e8dded66990a763112
CAAMCODE <85082707+CAAMCODE@users.noreply.github.com>
2021-05-30 20:08:41 -0500

    Update sabado1.py

--- UNIDAD3/grupode estudio/sabado1.py
+++ UNIDAD3/grupode estudio/sabado1.py
@@ -1,14 +1,4 @@
-""" diccionario = {}
-palabras = input("Introduce la lista de palabras y traducciones en formato palabra:traducción separadas por comas: ")
-for i in palabras.split(','):
-     clave, valor = i.split(':')
-     diccionario[clave] = valorcat
-frase = input('Introduce una frase en español: ')
-for i in frase.split():
-    if i in diccionario:
-        print(diccionario[i], end=' ')
-    else:
-        print(i, end=' ') """
+
 
 
 
